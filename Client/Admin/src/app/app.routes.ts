1d5ed2dff8a53eaf2b0d2d8296c45cdffadecead
Anonymous <usermanagement@gmail.com>
2025-06-24 14:34:51 +0800

    adding dashboard components after logging in in admin side

--- Client/Admin/src/app/app.routes.ts
+++ Client/Admin/src/app/app.routes.ts
@@ -1,9 +1,11 @@
 import { Routes } from '@angular/router';
 import { AdminLoginComponent } from './admin-login/admin-login.component';
 import { AdminRegisterComponent } from './admin-register/admin-register.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
 
 export const routes: Routes = [
   { path: '', component: AdminLoginComponent },
   { path: 'admin/register', component: AdminRegisterComponent },
+  { path: 'dashboard', component: DashboardComponent },
   // ...other routes
 ];
